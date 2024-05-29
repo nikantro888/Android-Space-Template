096c773e86a70a6de76326560a190f0f2e523958
Nikoloz Khidesheli <81643116+nikantro888@users.noreply.github.com>
2024-05-29 16:38:09 +0400

    # Updated settings.gradle.kts by adding build logic, toml and other repos path

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,29 @@
+apply(from = file("$rootDir/git-utils.gradle.kts"))
+
+include(":Template")
+
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
         mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
         google()
-        mavenCentral()
+        gradlePluginPortal()
+
+        maven { url = uri("https://plugins.gradle.org/m2/") }
     }
 }
 
-rootProject.name = "Template"
-include(":app")
- 
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        google()
+        jcenter()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("build-logic/gradle/libs.versions.toml"))
+        }
+    }
+}
